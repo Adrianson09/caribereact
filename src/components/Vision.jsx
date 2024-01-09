b50e15803582a8ed2415eb83cdc2e2c603c763ee
Adrian Guerrero <adrianson@outlook.com>
2024-01-09 16:19:45 -0600

    change projects

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -11,7 +11,7 @@ const Vision = () => {
     <>
     <Suspense fallback="Cargando Traducciones">
 
-        <div id="vision" className=" p-20 mb-20  flex-row justify-center items-center text-center ">
+        <div id="vision" className=" p-20 mb-10  flex-row justify-center items-center text-center ">
             <h1 className="border-b-4 border-[#6AAB33] mb-4 pb-4 text-4xl font-bold text-[#6AAB33]">{t("about")}</h1>
             <div className="grid sm:grid-cols-2 mx-auto gap-10">
               <div>
@@ -31,7 +31,7 @@ const Vision = () => {
         </div>
 
 {/* Sostenibilidad */}
-        <div className="bg-[#6AAB33] p-20 mb-20 text-white relative">
+        {/* <div className="bg-[#6AAB33] p-20 mb-20 text-white relative">
         <h1 id="sostenibilidad" className=" mb-4 pb-4 text-2xl sm:text-4xl font-bold ">{t("sostenibilidad-heading")}</h1>
             <div className="grid sm:grid-cols-2 mx-auto sm:px-10  gap-10">
               <div>
@@ -48,11 +48,11 @@ const Vision = () => {
               </div>
         </div>
         <img className=" absolute sm:top-10 top-6 right-10 " src="/img/sostenibilidad.svg" alt="Sostenibilidad " width={100} height={200} />
-        </div>
+        </div> */}
 
 {/* Sostenibilidad */}
 
-        <div className=" p-20 mb-10  relative">
+        <div className=" p-20 mb-10   relative">
         <h1 id="sostenibilidad" className=" text-[#6AAB33] mb-4 pb-4 text-center text-2xl sm:text-4xl font-bold ">{t("sostenibilidad-heading")}</h1>
             <div className="grid sm:grid-cols-2 mx-auto sm:px-10  gap-10">
               <div>
